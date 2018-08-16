637e24a0331b5f2b9f59276784b97789537f0eff
PlatonJKE <platon69@bk.ru>
2018-08-16 15:18:59 +0300

    first commit

---
+++ src/main/java/com/javamentor/kidstarter/KidstarterApplication.java
@@ -0,0 +1,12 @@
+package com.javamentor.kidstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidstarterApplication.class, args);
+	}
+}
