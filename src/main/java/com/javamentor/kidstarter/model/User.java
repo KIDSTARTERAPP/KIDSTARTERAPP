97ea4069a28ec93aa6cd87ce36e8dd9c6ae652f1
PlatonJKE <platon69@bk.ru>
2018-08-16 15:59:42 +0300

    add user model

---
+++ src/main/java/com/javamentor/kidstarter/model/User.java
@@ -0,0 +1,24 @@
+package com.javamentor.kidstarter.model;
+
+public class User {
+
+	private Long id;
+
+	private String password;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String sex;
+
+	private int phoneNumber;
+
+	private String country;
+
+}
+
+
+
+
+
