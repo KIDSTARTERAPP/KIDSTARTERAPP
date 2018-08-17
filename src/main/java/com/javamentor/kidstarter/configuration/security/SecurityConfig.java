f5f014370e93093d326af069536cd33518fb86e6
Mikhail Shonorov <mailto:shonorov@gmail.com>
2018-08-17 12:20:47 +0300

    static resources filter added

--- src/main/java/com/javamentor/kidstarter/configuration/security/SecurityConfig.java
+++ src/main/java/com/javamentor/kidstarter/configuration/security/SecurityConfig.java
@@ -15,12 +15,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/main");
+		web.ignoring().antMatchers("/main", "/scripts/**", "/style/**");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/main").permitAll()
+		http.authorizeRequests()
+				.antMatchers("/main", "/scripts/**", "/styles/**").permitAll()
 				.anyRequest().authenticated()
 				.and().formLogin().defaultSuccessUrl("/main")
 				.and().httpBasic()
