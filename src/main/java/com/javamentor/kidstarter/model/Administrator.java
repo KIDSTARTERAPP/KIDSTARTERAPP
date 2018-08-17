69d1e2b7a9b93d26df85dd9889e7b5a7af7d43c3
Mikhail Shonorov <mailto:shonorov@gmail.com>
2018-08-17 14:32:47 +0300

    top 5 models

--- src/main/java/com/javamentor/kidstarter/model/Administrator.java
+++ src/main/java/com/javamentor/kidstarter/model/Administrator.java
@@ -2,12 +2,14 @@ package com.javamentor.kidstarter.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Data
+@EqualsAndHashCode (callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- src/main/java/com/javamentor/kidstarter/model/Kid.java
+++ src/main/java/com/javamentor/kidstarter/model/Kid.java
@@ -1,8 +1,20 @@
 package com.javamentor.kidstarter.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Set;
 
-
+@Data
+@EqualsAndHashCode (callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "jury")
 public class Kid extends User {
 
     private Set<Job> jobInterest;
