b67c707ed82bd08c0dd41e465353cef7348eb0c7
Sorokinsta <sorokin.sta@gmail.com>
2018-08-17 19:55:17 +0300

    Delete all

--- src/main/java/com/javamentor/kidstarter/dao/interfaces/RoleDao.java
+++
@@ -1,8 +0,0 @@
-package com.javamentor.kidstarter.dao.interfaces;
-
-import com.javamentor.kidstarter.model.Role;
-
-
-public interface RoleDao extends GenericDao<Long,Role> {
-    Role getByName(String name);
-}
--- src/main/java/com/javamentor/kidstarter/dao/interfaces/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.kidstarter.dao.interfaces;
-
-import com.javamentor.kidstarter.model.User;
-
-
-public interface UserDao extends GenericDao<Long , User> {
-
-    User getByLogin(String login);
-}
