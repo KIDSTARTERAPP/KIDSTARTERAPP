9a48e4f1fb2526accd6a08d2e4526d67312b65a6
Mikhail Shonorov <mailto:shonorov@gmail.com>
2018-08-27 13:25:39 +0300

    copied user files

--- src/main/java/com/javamentor/kidstarter/controller/KidController.java
+++ src/main/java/com/javamentor/kidstarter/controller/KidController.java
@@ -2,19 +2,17 @@ package com.javamentor.kidstarter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/kid")
 public class KidController {
 
-    @GetMapping("/jobs")
-    public String showKidWishList() {
+    @GetMapping("/kid")
+    public String showKidPage() {
         return "kid_page";
     }
 
-    @GetMapping("/pick-jobs")
-    public String showKidPickJobPage() {
+    @GetMapping("/kid/jobs")
+    public String showKidJobPickPage() {
         return "kid_pick_jobs";
     }
 }
