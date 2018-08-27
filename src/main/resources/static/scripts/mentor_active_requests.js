ac12b5664c0cc69b1716de0653de3c8fc37d6fa1
Mikhail Shonorov <mailto:shonorov@gmail.com>
2018-08-28 01:29:22 +0300

    Teach free button

--- src/main/resources/static/scripts/mentor_active_requests.js
+++ src/main/resources/static/scripts/mentor_active_requests.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
     fill_active_requests_table();
 });
 
-
-
 function fill_active_requests_table() {
     $("#table_active_requests").empty();
     $.ajax({
@@ -21,10 +19,47 @@ function fill_active_requests_table() {
                     $('<td>').text(item.price / 100),
                     $('<td>').text(item.status)
                 );
+                if (item.status == "IN_PROGRESS") {
+                    var tr = $('<tr>').append(
+                        $('<td>').text(item.id),
+                        $('<td>').text(item.description),
+                        $('<td>').text(item.job.name),
+                        $('<td>').text(item.kids.length),
+                        $('<td>').text(item.price / 100),
+                        $('<td>').text(item.status),
+                        $('<td>').append("<form onsubmit='get_free_request(" + item.id + ");return false'><input class='btn btn-lg btn-primary btn-block' type='submit' value='Учить бесплатно'></form>")
+                    );
+                }
                 if ((item.status != "NEW") && (item.mentor.user.id == current_user.id)) {
                     $("#table_active_requests").append(tr);
                 }
             });
+        }
+    })
+}
+
+function get_free_request(id) {
+    var url = "/api/request/" + id;
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "JSON",
+        success: function (response) {
+            response.status = "PAID";
+            teach_free(JSON.stringify(response));
+        }
+    })
+}
+
+function teach_free(data) {
+    $.ajax({
+        type: "PUT",
+        url: "/api/request",
+        contentType : "application/json; charset=UTF-8",
+        dataType: "JSON",
+        data:data,
+        success: function () {
+            fill_active_requests_table();
         }
     })
 }
