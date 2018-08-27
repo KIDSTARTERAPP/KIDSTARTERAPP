4458121965046278ce409cf2b5625313e494b9c2
Mikhail Shonorov <mailto:shonorov@gmail.com>
2018-08-27 14:37:34 +0300

    Kid to organization link fixed

--- src/main/java/com/javamentor/kidstarter/model/user/Organization.java
+++ src/main/java/com/javamentor/kidstarter/model/user/Organization.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -54,5 +55,6 @@ public class Organization {
         this.phone = phone;
         this.createDate = createDate;
         this.account = account;
+        this.kids = new HashSet<>();
     }
 }
