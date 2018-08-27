cfcd82cd2aba9257cc2ec4b41cb908d585dbc3e7
Egor <yaegorko@gmail.com>
2018-08-27 18:43:23 +0300

    Yandex Form API complite

---
+++ src/main/java/com/javamentor/kidstarter/service/Impl/YandexMoneyServiceImpl.java
@@ -0,0 +1,15 @@
+package com.javamentor.kidstarter.service.Impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class YandexMoneyServiceImpl {
+
+    private void sendPost() {
+
+    }
+}
